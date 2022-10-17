ad764794f19ec67115079c2d282a289343eb6f58
Vasil Valchev <96626766+VsVch@users.noreply.github.com>
2022-10-17 17:57:53 +0300

    Add files via upload

---
+++ JS Advanced/Syntax, Functions and Statements - Lab/02. String Length/02. String Length.js
@@ -0,0 +1,11 @@
+function threeWords(first, second, third){
+    let sum = first.length + second.length + third.length;
+    let average = Math.floor (sum / 3);
+
+    console.log (sum);
+    console.log(average)
+
+}
+threeWords('chocolate', 'ice cream', 'cake');
+
+

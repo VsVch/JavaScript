a957ac5fea4d1e0f99ab176512e69039050a3dc2
Vasil Valchev <96626766+VsVch@users.noreply.github.com>
2022-10-21 08:29:41 +0300

    Add files via upload

---
+++ JS Advanced/Arrays and Nested Arrays - Lab/03. Sum First Last/03. Sum First Last .js
@@ -0,0 +1,7 @@
+function sumFirstLast(array){
+    let first = Number(array.shift());
+    let second = Number(array.pop());
+
+    return first + second;
+}
+console.log(sumFirstLast(['20', '30', '40']));

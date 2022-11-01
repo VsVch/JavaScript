9ab6a4ee48402b315f474b97f7ddb2ccdd9721f3
Vasil Valchev <96626766+VsVch@users.noreply.github.com>
2022-11-01 07:33:31 +0200

    Add files via upload

---
+++ JS Advanced/DOM Manipulation and Events - Exercise/01. Sections/script.js
@@ -0,0 +1,19 @@
+function create(words) {
+   let divsArray = [];
+   let contentElement = document.getElementById('content');
+
+   let displayParagraph = function (e) {
+      e.target.firstChild.style.display = 'block';
+   }
+   words.forEach(el => {
+      divElement = document.createElement('div');
+      divElement.addEventListener('click', displayParagraph);
+
+      paragraphElement = document.createElement('p');
+      paragraphElement.textContent = el;
+      paragraphElement.style.display = 'none';
+
+      divElement.appendChild(paragraphElement);
+      contentElement.appendChild(divElement);
+   });
+}

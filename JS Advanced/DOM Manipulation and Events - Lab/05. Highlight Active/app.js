21da8f5a5d6114255d22bd761e20613541a18ab2
Vasil Valchev <96626766+VsVch@users.noreply.github.com>
2022-11-01 07:29:49 +0200

    Add files via upload

---
+++ JS Advanced/DOM Manipulation and Events - Lab/05. Highlight Active/app.js
@@ -0,0 +1,17 @@
+function focused() {
+    let divElements = document.querySelectorAll('div input');
+
+    for (const divElement of divElements) {
+
+        divElement.addEventListener('focus', (e)=>{
+            e.target.parentNode.className = 'focused';            
+        })
+
+        divElement.addEventListener('blur', (e)=>{
+            e.target.parentNode.className = '';            
+        })
+        
+        
+    }
+}
+

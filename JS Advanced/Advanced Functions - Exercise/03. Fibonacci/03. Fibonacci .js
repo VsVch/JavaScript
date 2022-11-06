d24238f3909cc7757adebb18dc9e09dfadf7b85a
Vasil Valchev <96626766+VsVch@users.noreply.github.com>
2022-11-06 12:32:14 +0200

    Add files via upload

---
+++ JS Advanced/Advanced Functions - Exercise/03. Fibonacci/03. Fibonacci .js
@@ -0,0 +1,24 @@
+function main() {
+
+    let prev = 0;
+    let curr = 1;
+
+    return function fib(){
+
+        let next = prev + curr;
+        prev = curr;
+        curr = next;
+
+        return prev;
+    }
+
+}
+
+let fib = main();
+console.log(fib()); // 1
+console.log(fib()); // 1
+console.log(fib()); // 2
+console.log(fib()); // 3
+console.log(fib()); // 5
+console.log(fib()); // 8
+console.log(fib()); // 13
